77a6628ce2f27ded1ff958c2f53537ba9ba99159
lizook3450 <123293342+lizook3450@users.noreply.github.com>
2023-02-28 22:29:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 /* Генерация предсказания должна происходить при клике на кнопку «предсказать судьбу» */
-
-/* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы (или что скажет Math.random) показывай их пользователю */
+/* Заранее заготовь 3-5 предсказаний и в зависимости от того, как лягут карты судьбы 
+(или что скажет Math.random) показывай их пользователю */
 
 /* Подставляй текст нового предсказания в .current-forecast h1 */
 
@@ -8,8 +8,48 @@
 
 /* Данный процент также нужно генерировать автоматически, он может принимать значения от 0 до 100% */
 
-/* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне от min до max и используй ее где нужно */
+/* Совет: заведи функцию-хелпер, которая будет заниматься только генерацией данных в диапазоне
+ от min до max и используй ее где нужно */
 
-/* При генерации нового предсказания старое предсказание должно добавляться в начало списка «Мои предсказания» — .forecasts  */
+/* При генерации нового предсказания старое предсказание должно добавляться в начало списка 
+«Мои предсказания» — .forecasts  */
 
 /* Для добавления предсказания в список воспользуйся шаблоном forecast-item */
+
+const forecastButton = document.querySelector('.forecast-btn');
+const container = document.querySelector(".forecasts");
+const currentForecast = document.querySelector('.current-forecast');
+const forecastItem = document.querySelector('#forecast-item');
+forecastButton.addEventListener('click', function() {
+
+    function generateRandomInt(min, max) {
+        return Math.floor(Math.random() * (max - min)) + min;
+    };
+
+    const number = generateRandomInt(1, 4);
+    let prediction = 'Всё будет хорошо!';
+    switch(number) {
+      case 1:
+        prediction = 'Сегодня тебя ждёт приятный сюрприз!';
+        break;
+      case 2:
+        prediction = 'Совсем скоро ты начнёшь путешестовать!';
+        break;
+    }
+   
+    currentForecast.querySelector('h1').textContent = prediction;
+
+    const percentage = Math.floor(Math.random() * 100) + "%";
+    currentForecast.querySelector('p').textContent = percentage;
+    const firstForecast = makeForecastByTemplate(prediction, percentage);
+    container.append(firstForecast);
+
+});
+
+function makeForecastByTemplate(header, percentage) {
+    const forecast = forecastItem.content.cloneNode(true);
+    forecast.querySelector('h3').textContent = header;
+    forecast.querySelector('p').textContent = percentage;
+
+    return forecast;
+};
